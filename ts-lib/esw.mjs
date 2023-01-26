bc9a0dc42aa76183ac256053f146cd6186b86cc9
nsfxd <nsfxd@proton.me>
2023-01-27 07:49:29 +0800

    up

--- ts-lib/esw.mjs
+++ ts-lib/esw.mjs
@@ -1,20 +1,21 @@
-import {spawn} from "node:child_process"
-import esbuild from "esbuild"
+import {spawn} from 'node:child_process'
+import esbuild from 'esbuild'
 
+const filename = process.argv[2].split('/').at(-1).split('.').at(0)
 let SERVER
 function onEnd() {
-  if (SERVER) SERVER.kill("SIGINT")
-  SERVER = spawn("node", ["temp/app.js"], {stdio: "inherit"})
+  if (SERVER) SERVER.kill('SIGINT')
+  SERVER = spawn('node', [`temp/${filename}.js`], {stdio: 'inherit'})
 }
 
 const ctx = await esbuild.context({
   entryPoints: [process.argv[2]],
   bundle: true,
-  outdir: "temp",
-  platform: "node",
+  outdir: 'temp',
+  platform: 'node',
   plugins: [
     {
-      name: "onEnd",
+      name: 'onEnd',
       setup(b) {
         b.onEnd(onEnd)
       },
