443f2cd129dc11efdea9f89af7f199089f6a8e5b
nsfxd <nsfxd@proton.me>
2023-04-08 01:22:37 +0800

    remove build; now test only

--- ts-lib/esw.mjs
+++ ts-lib/esw.mjs
@@ -2,32 +2,25 @@ import {spawn} from "node:child_process"
 import esbuild from "esbuild"
 import glob from "glob"
 
-const IS_TEST = process.argv[2] === "test"
-const DIR = IS_TEST ? "temp" : "dist"
-
+const DIR = "temp"
 let SERVER
 function onEnd() {
   if (SERVER) SERVER.kill("SIGINT")
   SERVER = spawn("node", ["--test", DIR], {stdio: "inherit"})
 }
 
-const plugins = IS_TEST
-  ? [
-      {
-        name: "onEnd",
-        setup(b) {
-          b.onEnd(onEnd)
-        },
-      },
-    ]
-  : []
+const plugins = [
+  {
+    name: "onEnd",
+    setup(b) {
+      b.onEnd(onEnd)
+    },
+  },
+]
 
-const entryPoints = IS_TEST ? await glob("test/**/*.test.ts") : ["src/index.ts"]
-
+const entryPoints = await glob("test/**/*.test.ts")
 const ctx = await esbuild.context({
   entryPoints,
-  sourcemap: !IS_TEST,
-  minify: !IS_TEST,
   bundle: true,
   outdir: DIR,
   platform: "node",
@@ -35,4 +28,4 @@ const ctx = await esbuild.context({
   plugins,
 })
 
-IS_TEST && (await ctx.watch())
+await ctx.watch()
